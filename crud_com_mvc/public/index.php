3059c4bb03b0b4c9c02cebd20cbc3efb61d80f34
Josue Rosario <josue.rosario@escolar509.sp.senai.br>
2024-04-18 17:25:50 -0300

    cadastrar pessoa

--- crud_com_mvc/public/index.php
+++ crud_com_mvc/public/index.php
@@ -1,6 +1,6 @@
 <?php
     require_once "../vendor/autoload.php";
     $route = new App\Route;
-    echo '<hr>';
-    print_r($route->getUrl());
+    // echo '<hr>';
+    // print_r($route->getUrl());
 ?>
